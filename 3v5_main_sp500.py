b3743eb4af694c4b64f307b1dc8eed6536bbc113
Ilya <ilya.ilyin5@gmail.com>
2022-05-23 22:52:36 +0300

    add some type annotations

--- 3v5_main_sp500.py
+++ 3v5_main_sp500.py
@@ -2,7 +2,7 @@ from connection_to_db import connect
 import datetime
 from update_all_data_db import normalise_lists_from_db
 from func_exec_and_read_querys_db import read_query_all
-
+from typing import *
 
 # connect to db
 conn = connect()
@@ -14,15 +14,15 @@ date_6m_ago = datetime.date.today() - datetime.timedelta(days=180)
 sp500list = normalise_lists_from_db(read_query_all(conn, 'SELECT symbol FROM sp500_list'))
 
 
-def find_key_by_value_in_dic(some_dict, value):
+def find_key_by_value_in_dic(some_dict: Dict, value):
     return list(some_dict.keys())[list(some_dict.values()).index(value)]
 
 
-def get_data(from_date, ticket):
+def get_data(from_date, ticket: str):
     return read_query_all(conn, f"""select date, volume from "{ticket}" where date > '{from_date}'""")
 
 
-def make_a_dic_simple(data_from_db):
+def make_a_dic_simple(data_from_db) -> Dict:
     dic_simple = {}
     for items in data_from_db:
         # dataset_dic[str(items.date.date())] = items.volume, items.open, items.high, items.low, items.close
@@ -30,11 +30,11 @@ def make_a_dic_simple(data_from_db):
     return dic_simple
 
 
-def list_of_values_from_dic(some_dic):
+def list_of_values_from_dic(some_dic: Dict) -> List:
     return [item for item in some_dic.values()]
 
 
-def average_value(some_list_of_value):
+def average_value(some_list_of_value: List):
     return int(sum(some_list_of_value) / len(some_list_of_value))
 
 
