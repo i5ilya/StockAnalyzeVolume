ffb1490aafa8bc99a4c9b139749ece210139f154
Ilya <ilya.ilyin5@gmail.com>
2022-05-27 23:03:49 +0300

    low cases in columns name, return list

--- price_and_vol_patterns.py
+++ price_and_vol_patterns.py
@@ -11,14 +11,14 @@ def volume_price_level_calc(pd_df):
     #  Делаем копию dataframe
     pd_df = pd_df.copy()
     #  Создаем новый столбец 'Price Groups', в котором все цены будут поделены на группы (количество групп: bins=30)
-    pd_df['Price Groups'] = pd.cut(pd_df['Close'], bins=30)
+    pd_df['Price Groups'] = pd.cut(pd_df['close'], bins=30)
     #  Группируем новую таблицу по столбцу 'Price Groups', при этом суммируем и объединяем все объемы по 'Price Groups'
-    pd_df = pd_df.groupby(['Price Groups'])['Volume'].sum().reset_index()
+    pd_df = pd_df.groupby(['Price Groups'])['volume'].sum().reset_index()
     # Сортируем новую таблицу по столбцу 'Volume', наибольшие объемы вверху таблицы
-    sorted_pd_df = pd_df.sort_values(by='Volume', ascending=False)
+    sorted_pd_df = pd_df.sort_values(by='volume', ascending=False)
     #  Делаем табличку short с тремя верхними диапазонами, которые имеют самые большие объемы
     short = sorted_pd_df.head(3)
     #  Выбираем нижнюю и верхнюю границы этих трех диапазонов цен
     max_price = short['Price Groups'].max()
     min_price = short['Price Groups'].min()
-    return min_price.left, max_price.right
+    return [min_price.left, max_price.right]
