082171bfb4fae74a9f316d0d574be9586e88a075
Ilya <ilya.ilyin5@gmail.com>
2022-06-08 19:40:41 +0300

    add in dl_data_yf_period .dropna(): if in pandas dataframe in column not number, pandas return na in cell. '.dropna()' method remove string like that.

--- func_exec_and_read_querys_db.py
+++ func_exec_and_read_querys_db.py
@@ -98,17 +98,17 @@ def copy_from_stringio(conn, df, table):
 #         print(f"The error '{error}' occurred")
 
 
-def dl_data_yf_period(ticket, start_time, end_time, timeframe:str ='1d'):
-    '''
+def dl_data_yf_period(ticket, start_time, end_time, timeframe: str ='1d'):
+    """
     :param ticket: from Yahoo Finance
     :param start_time: Date start
     :param end_time: Date end
     :param timeframe: valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
     :return: pandas dataframe
-    '''
+    """
     data = None
     try:
         data = yf.download(ticket, start=start_time, end=end_time, interval=timeframe)
-        return data
+        return data.dropna()
     except Exception as error:
         print(f"The error '{error}' occurred")
