b2aa41cd632852dc3bb2d1d6a8ab9ff1a6765cd5
Ilya <ilya.ilyin5@gmail.com>
2022-06-26 19:58:02 +0300

    comment old code

--- connection.py
+++ connection.py
@@ -4,19 +4,19 @@ from io import StringIO
 
 # Connection parameters
 
-def connect():
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(dbname='SP500', user='postgres',
-                                password='syncmaster', host='localhost')
-        print("Connection to PostgreSQL DB successful")
-        return conn
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-        # sys.exit(1)
+# def connect():
+#     """ Connect to the PostgreSQL database server """
+#     conn = None
+#     try:
+#         # connect to the PostgreSQL server
+#         print('Connecting to the PostgreSQL database...')
+#         conn = psycopg2.connect(dbname='SP500', user='postgres',
+#                                 password='syncmaster', host='localhost')
+#         print("Connection to PostgreSQL DB successful")
+#         return conn
+#     except (Exception, psycopg2.DatabaseError) as error:
+#         print(error)
+#         # sys.exit(1)
 
 
 class Database:
