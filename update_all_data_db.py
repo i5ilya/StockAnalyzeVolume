1993f4b44864d7e870f5029a1a49cf37cbc5abb0
Ilya <ilya.ilyin5@gmail.com>
2022-06-26 19:41:21 +0300

    del old commented func

--- update_all_data_db.py
+++ update_all_data_db.py
@@ -1,58 +1,15 @@
 from connection import Tables
-from queries import query_get_list_of_tables
-from func_exec_and_read_querys_db import read_query_all, execute_query, copy_from_stringio, read_query_one, \
-    dl_data_yf_period, query_delete_duplicates
+from func_exec_and_read_querys_db import dl_data_yf_period, query_delete_duplicates
 from queries import query_get_list_of_tables, query_create_table
-import yfinance as yf
-import pandas as pd
 from datetime import datetime
 import datetime
-import time
-
 
 db = Tables('SP500')
+
 
 def normalise_lists_from_db(raw_list):
     return [item[0].lower() for item in raw_list]
 
-
-# def dl_data_from_yahoo_to_db(symbols_to_dl, symbols_in_database):
-#     for symbol in symbols_to_dl:
-#         if symbol not in symbols_in_database:
-#             print(f'Символа {symbol}, нет в базе данных, скачиваем...')
-#             df = yf.download(symbol, period='6mo')
-#             print(f'Создаем таблицу {symbol}...')
-#             execute_query(conn, query_create_table(symbol))
-#             print(f'Заполняем таблицу данными {symbol}...')
-#             copy_from_stringio(conn, df, symbol)
-#         else:
-#             print(f'Символ {symbol} присутствует в базе, проверяем последнюю запись в базе...')
-#             last_date_raw = read_query_one(conn, f'SELECT max(date) FROM "{symbol}"')
-#             last_date = last_date_raw[0].date()
-#             if last_date != datetime.date.today() and last_date != datetime.date.today() - datetime.timedelta(days=1):
-#                 if last_date is not None:
-#                     print(f'База данных {symbol} содержит последнюю запись от {last_date}')
-#                     #  days_plus_one_from_db = last_date[0] + datetime.timedelta(days=1) # если грузим повторно в рабочий день, то день с заврашний)
-#                     print(f'Скачиваем данные с {last_date} по {datetime.date.today()}... ')
-#                     df_d = dl_data_yf_period(symbol, last_date, datetime.date.today())
-#                     print(f'Загружаем полученные данные по {symbol} в БД...')
-#                     copy_from_stringio(conn, df_d, symbol)
-#                     print(f'Удаляем возможные дубли в базе {symbol}')
-#                     execute_query(conn, query_delete_duplicates(symbol, 'date'))
-#                 else:
-#                     print(f'База данных {symbol} существует, но она пустая! Пробуем скачать данные еще раз...')
-#                     df = yf.download(symbol, period='6mo')
-#                     print(f'Заполняем таблицу данными {symbol}...')
-#                     copy_from_stringio(conn, df, symbol)
-#                     last_date = read_query_one(conn, f'SELECT max(date) FROM {symbol}')
-#                     if last_date is not None:
-#                         print(f'Кажется, база заполнилась)')
-#                     else:
-#                         print(f'Какие-то проблемы с загрузкой {symbol}')
-#                         problem_with_data_load.append(symbol)
-#             else:
-#                 print(f'База данных {symbol} содержит последнюю запись от {last_date}')
-#                 print(f'Данные {symbol} в БД актуальны и не требуют загрузки')
 
 def dl_data_to_db(symbol, symbols_in_database, date_from, date_to, tf='1d'):
     """
@@ -134,4 +91,3 @@ if __name__ == '__main__':
 
     # if len(list_tables_not_in_db) != 0:
     #    print(f'Все еще остались не созданы в БД {list_tables_not_in_db}')
-
