9c027e39bf0e6ff497885efb7b5cef32a03b57ba
Ilya <ilya.ilyin5@gmail.com>
2022-08-01 20:03:54 +0300

    update connection

--- update_sp500_symbols_db.py
+++ update_sp500_symbols_db.py
@@ -1,9 +1,10 @@
-from connection import connect
+#from connection import connect
+from connection import Tables
 from func_exec_and_read_querys_db import read_query_all, execute_query, copy_from_stringio
 import pandas as pd
 
-conn = connect()
-
+#conn = connect()
+db = Tables('SP500')
 
 def normalise_lists_from_db(raw_list):
     return [item[0] for item in raw_list]
@@ -14,7 +15,7 @@ df = table[0]
 dfy = df['Symbol'].str.replace('.', '-',
                                regex=True)  # У yahoo finance в некоторых тикерах символы с точкой идут через тире.
 
-symbols_sp500 = normalise_lists_from_db(read_query_all(conn, 'SELECT symbol FROM sp500_list'))
+symbols_sp500 = normalise_lists_from_db(db.fetch_all('SELECT symbol FROM sp500_list'))
 symbols_in_wiki = dfy.tolist()
 
 for symbol in symbols_in_wiki:
@@ -28,12 +29,12 @@ for symbol in symbols_sp500:
 try:
     value = int(input('Для обновления списка акций из вики в базу нажми "1": '))
     if value == 1:
-        execute_query(conn, 'delete from sp500_list')
-        copy_from_stringio(conn, dfy, 'sp500_list')
+        db.execute_query('delete from sp500_list')
+        db.copy_from_stringio(dfy, 'sp500_list')
         for symbol in symbols_sp500:
             if symbol not in symbols_in_wiki:
                 print(f'{symbol} удален из списка на википедии, удаляем из базы')
-                execute_query(conn, f'DROP TABLE IF EXISTS "{symbol.lower()}_1d", "{symbol.lower()}_5m"')
+                db.execute_query(f'DROP TABLE IF EXISTS "{symbol.lower()}_1d", "{symbol.lower()}_5m"')
     else:
         print('Ничего не делаем')
 except ValueError:
@@ -43,6 +44,6 @@ except ValueError:
 # copy_from_stringio(conn, dfy, 'sp500_list')
 
 
-conn.close()
+# conn.close()
 # if __name__ == '__main__':
 #     cursor = conn.cursor()
